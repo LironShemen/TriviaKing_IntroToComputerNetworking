03295f8b346343c2433f110f1a8ba3c3d648408c
LironShemen <lironmir@post.bgu.ac.il>
2024-04-08 20:28:18 +0300

    change listen for offers in client

--- TriviaKIng/Client.py
+++ TriviaKIng/Client.py
@@ -35,20 +35,37 @@ class TriviaGameClient:
     def listen_for_offers(self, udp_socket):
         print("Client started, listening for offer requests...")
 
+        # while True:
+        #     ready_sockets, _, _ = select.select([udp_socket], [], [], 1)
+        #
+        #     if udp_socket in ready_sockets:
+        #         data, addr = udp_socket.recvfrom(self.buffer_size)
+        #         message = data.decode('utf-8')
+        #         self.server_port = addr[1]
+        #         self.handle_offer(message[4:36].strip(), addr[0])
+        #
+        #     if self.state == "connecting_to_server":
+        #         self.connect_to_server()
+        #
+        #     if self.state == "game_mode":
+        #         self.game_mode()
         while True:
-            ready_sockets, _, _ = select.select([udp_socket], [], [], 1)
-
-            if udp_socket in ready_sockets:
+            try:
                 data, addr = udp_socket.recvfrom(self.buffer_size)
                 message = data.decode('utf-8')
-                self.server_port = addr[1]
-                self.handle_offer(message[4:36].strip(), addr[0])
+                server_name = message[4:36].strip()  # Extract server name
+                self.server_port = int.from_bytes(data[36:38], 'big')  # Extract server port
+                self.handle_offer(server_name, addr[0])
 
-            if self.state == "connecting_to_server":
-                self.connect_to_server()
+                if self.state == "connecting_to_server":
+                    self.connect_to_server()
 
-            if self.state == "game_mode":
-                self.game_mode()
+                if self.state == "game_mode":
+                    self.game_mode()
+
+            except socket.timeout:
+                print("No more offer requests received. Exiting...")
+                break
 
     #Handles the offer received from the server. It sets the server address and changes
     # the state to "connecting_to_server".
