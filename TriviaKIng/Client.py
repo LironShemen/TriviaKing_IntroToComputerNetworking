c251df4722a43834c527a650559c776ac01970b5
SapirTzaig <tzaigsap@post.bgu.ac.il>
2024-04-10 18:49:22 +0300

    fixing question errors

--- TriviaKIng/Client.py
+++ TriviaKIng/Client.py
@@ -89,21 +89,12 @@ class TriviaGameClient:
                     self.state = "looking_for_server"
                     self.listen_for_offers(self.udp_socket)
                 if decoded_data.startswith("Qusetion: "):
-                    # Wait for user input with a timeout of 1 second
-                    event = keyboard.wait("down", timeout=10)
-                    if event:
+                    event = keyboard.read_event()
+                    keyboard.press("enter")
+                    if event.event_type == keyboard.KEY_DOWN:
                         key = event.name
-                        print("\n")
-                        if key.strip():  # Check if the answer is not empty (user input something)
-                            self.tcp_socket.sendall(key.encode())  # Send the answer to the server
-                        else:  # If the user didn't input an answer
-                            self.tcp_socket.sendall(b"\n")
-                    # key = keyboard.read_event().name
-                    # print("\n")
-                    # if key.strip():  # Check if the answer is not empty (user input something)
-                    #     self.tcp_socket.sendall(key.encode())  # Send the answer to the server
-                    # else:  # If the user didn't input an answer
-                    #     self.tcp_socket.sendall(b"\n")
+                    print("\n")
+                    self.tcp_socket.sendall(key.encode())
             except:
                 pass
 
